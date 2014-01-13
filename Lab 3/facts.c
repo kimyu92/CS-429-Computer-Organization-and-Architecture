a39151033fb3f906b64778b97ebb0572dfc6876d
KimYu Ng <kimyu92@cs.utexas.edu>
2014-01-12 23:03:41 -0600

    lab 3.11

--- Lab 3/facts.c
+++ Lab 3/facts.c
@@ -23,11 +23,6 @@ char *arrStr;
 char *res;
 
 
-String obj;
-String prop;
-String val;
-
-
 //Functions scope
 void process();
 
@@ -39,77 +34,33 @@ char* deblank(char* input);
 void get_a_fact(String thisString);
 char *substr(const char *data_str, int pos_start, int pos_end);
 
-void create_the_fact_array();
-void resize_the_fact_array();
+//Search the linkedlist and update
+void search_in_linkedlist();
 
-//Testing/Development
-void testing_linkedlist();
-//================================
-//End of Declaration
-//================================
+//==================================
+//End of Declaration without struct
+//==================================
 
 
-
-//Struct
-//ListNode
-struct ListNode{
+//Struct of ListNode
+typedef struct ListNode{
     char *object_data;
     char *property_data;
     char *value_data;  
-    struct ListNode *next;              //the next node
-}*root = NULL, *temp;
+    
+    //The next node
+    struct ListNode *next;
+}ListNode;
 
+ListNode *root = NULL;
 
 //Method prototype from strct
 void display(struct ListNode *r);
 void addNode(struct ListNode *temp);
 
-
-//Print of linkedlist
-void display(struct ListNode *r){
-    int i  = 0; 
-    r = root;
-    if(r == NULL)
-        return;
-    
-    while(r != NULL){
-        printf("\n Node %d: ", i);
-        printf("%s ",r->object_data);
-        printf("%s ",r->property_data);
-        printf("%s ",r->value_data);
-        r=r->next;
-
-        i++;
-    }
-
-    printf("\n");
-}
-
-
-//Testing 
-void testing_linkedlist(){
-    //struct ListNode *temp;
-
-    //root = (struct ListNode*)malloc(sizeof(struct ListNode));
-    //temp = (struct ListNode*)malloc(sizeof(struct ListNode));
-    
-
-    // temp->object_data = "A";
-    // temp->property_data = "PropA";
-    // temp->value_data = "Cool";
-
-    // if (root == NULL){
-    //     root = temp;
-    //     root->next=NULL;
-    // }
-    // else{
-    //     temp->next=root;
-    //     root = temp;
-    // }
-
-    // display(temp);
-
-}
+//==================================
+//End of Declaration of struct
+//==================================
 
 
 
@@ -171,11 +122,7 @@ int main(int argc, char **argv){
 //Main Process
 void process(){
     //Get a line
-    testing_linkedlist();
     somehow_get_a_line();
-
-    //From a line start to process
-    //get_a_fact();
 }
 
 
@@ -189,9 +136,8 @@ char *somehow_get_a_line(){
     int charInLine = 0;
     int maxCharInLine = 0;
 
- 
-    
- 
+
+    //Read byte by byte to get the length of longest line 
     while ( (aByte = fgetc(input)) != EOF){
  
         //Switch on before a line
@@ -228,31 +174,9 @@ char *somehow_get_a_line(){
  
     //Allocate memory for fgets
     arrStr = (char*) malloc( (sizeof(char)) * (maxCharInLine + 1) );
-    free(arrStr);
-
      
     //Go again my size
-    if ( (fgets(arrStr, (maxCharInLine + 1), input)) != NULL ){
-
-        //Skip a blank line
-        if(arrStr[0] != '\n' ){
-
-            printf("This is before     : %s\n", arrStr);                           //Debugging
-
-            //Cutting down the string
-            arrStr = deblank(arrStr);
-        
-            printf("What is the truncated: %s\n", arrStr);                         //Debugging
-
-            //Getting the fact
-            temp = (struct ListNode*)malloc(sizeof(struct ListNode));
-
-            get_a_fact(arrStr);
-        }
-
-    }
-
-    if ( (fgets(arrStr, (maxCharInLine + 1), input)) != NULL ){
+    while ( (fgets(arrStr, (maxCharInLine + 1), input)) != NULL ){
 
         //Skip a blank line
         if(arrStr[0] != '\n' ){
@@ -288,8 +212,27 @@ char *somehow_get_a_line(){
 
     // }
 
+    // if ( (fgets(arrStr, (maxCharInLine + 1), input)) != NULL ){
+
+    //     //Skip a blank line
+    //     if(arrStr[0] != '\n' ){
+
+    //         printf("This is before     : %s\n", arrStr);                           //Debugging
+
+    //         //Cutting down the string
+    //         arrStr = deblank(arrStr);
+        
+    //         printf("What is the truncated: %s\n", arrStr);                         //Debugging
+
+    //         //Getting the fact
+    //         get_a_fact(arrStr);
+    //     }
+
+    // }
+
 
 }
+
 
 
 //Cutdown the string
@@ -308,12 +251,10 @@ char* deblank(char* input){
         j++;
     }                           
     
-
     output[j] = 0;
 
     return output;
 }
-
 
 
 
@@ -333,7 +274,6 @@ void get_a_fact(String thisString){
     if (s[0] != 'F')
         return;
 
-
     for (i = 0; i < len; i++){
         if (s[i] == ':'){
             pos_colon = i;
@@ -352,32 +292,22 @@ void get_a_fact(String thisString){
         return;
     }
 
-    //Instantiate array
-    create_the_fact_array();
 
-    
-
-    // printf("%d\n", pos_colon);
-    // printf("%d\n", pos_equ);
     printf("Result: \n");                               //Debugging starts
 
     
-    
+    ListNode *temp = (ListNode*)malloc(sizeof(ListNode));
 
     //Grabbing each element
-    object_name = substr(s, 1, pos_colon);
-    printf("%s\n", object_name);
-    printf("%lu\n", (sizeof(object_name)) );
-    
-
+    object_name = substr(s, 1, pos_colon);    
     temp->object_data = object_name;
-    printf("Error here \n");
-    //free(res);
+    printf("%s\n", object_name);
+
 
     property_name = substr(s, pos_colon + 1, pos_equ);
-    printf("%s\n", property_name);
     temp->property_data = property_name;
-    //free(res);
+    printf("%s\n", property_name);
+
 
     value_name = substr(s, pos_equ + 1, len);
     temp->value_data = value_name;
@@ -385,64 +315,77 @@ void get_a_fact(String thisString){
     //free(res);
 
     addNode(temp);
-    //free(res);
+    free(res);
+    free(temp);
 }
 
 
-void addNode(struct ListNode *temp){
-    struct ListNode *temptemp;
 
-    printf("TTTTT   \n");
-    //temptemp = (struct ListNode*)malloc(sizeof(struct ListNode));
-    printf("TTTTT   \n");
+//Print of linkedlist
+void display(struct ListNode *r){
+    int i  = 0; 
+    r = root;
+    if(r == NULL)
+        return;
+    
+    while(r != NULL){
+        printf("\n Node %d: ", i);
+        printf("%s ",r->object_data);
+        printf("%s ",r->property_data);
+        printf("%s ",r->value_data);
+        r=r->next;
+
+        i++;
+    }
+
+    printf("\n");
+}
+
+
+
+void addNode(struct ListNode *temp){
+    
+    ListNode *newNode = (ListNode*) malloc(sizeof(ListNode));
+
+    newNode->object_data = temp->object_data;
+    newNode->property_data = temp->property_data;
+    newNode->value_data = temp->value_data;
+    temp->next = NULL;
+    
+    
     if (root == NULL){
-        root = temp;
-        root->next = NULL;
+        root = newNode;
     }
     else{
-        temptemp = root;
+        ListNode *temptemp = root;
         
         while(temptemp->next != NULL)
             temptemp = temptemp->next;
         
-        temptemp->next = temp;
-        temp->next = NULL;
-
-        
-        //root = temptemp;
-
-        //display(temptemp);
-
+        temptemp->next = newNode;
     }
 
     display(root);
 }
 
 
+
 //Helper for get_a_fact
-//Fetch the data/value
+//Fetch the data / value
 char *substr(const char *data_str, int pos_start, int pos_end){
     int amount = pos_end - pos_start;
-    
-    //printf("HERE i am   \n");
-    //char *res = (char*) malloc( (sizeof(char)) * (amount + 1) );
+
     res = (char*) malloc( (sizeof(char)) * (amount+1) );
-    //printf("HERE i am v2  \n");
-
-    //printf("HERE i am v3  \n");
+    
     memcpy(res, data_str + pos_start, amount);
-    //printf("HERE i am v4  \n");
-
 
     return res;
 }
 
-void create_the_fact_array(){
-
-}
 
 
-void resize_the_fact_array(){
+//Search the object name and update the value
+void search_in_linkedlist(){
 
 
 
